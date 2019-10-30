ccbda281d66d99c0b200626fbad9c56c14cedac6
kooooohe <1211lta+github@gmail.com>
2019-10-30 16:54:26 +0900

    fixed bugs

--- code.ts
+++ code.ts
@@ -1,19 +1,43 @@
 function doPost(e) {
-  //name,company_name,mail,tel,preferred_visit_date,remarks
-  const keys: string[] = [
-    "name",
-    "company_name",
-    "mail",
-    "tel",
-    "preferred_visit_date",
-    "remarks"
-  ];
-  let addData: any[] = [];
-  for (let key of keys) {
-    addData.push(e.parameters[key]);
-  }
-  const sheet = SpreadsheetApp.getActive().getSheetByName("testGas");
+  try {
+    const keys: string[] = [
+      "name",
+      "company_name",
+      "mail",
+      "tel",
+      "preferred_visit_date",
+      "remarks"
+    ];
+    let addData: any[] = [];
+    for (let key of keys) {
+      if (e.parameter[key]) {
+        addData.push(e.parameter[key]);
+        continue;
+      }
+      addData.push("");
+    }
+    const sheet = SpreadsheetApp.getActive().getSheetByName("testGas");
+    // シートへの書き込み、getRange(開始行、開始列、行数、列数)
+    sheet.appendRow(addData);
 
-  // シートへの書き込み、getRange(開始行、開始列、行数、列数)
-  sheet.appendRow(addData);
+    const result = {
+      message: "success"
+    };
+    const out = ContentService.createTextOutput();
+    //Mime TypeをJSONに設定
+    out.setMimeType(ContentService.MimeType.JSON);
+    //JSONテキストをセットする
+    out.setContent(JSON.stringify(result));
+
+    return out;
+  } catch (error) {
+    const result = {
+      message: "failed"
+    };
+    const out = ContentService.createTextOutput();
+    //Mime TypeをJSONに設定
+    out.setMimeType(ContentService.MimeType.JSON);
+    //JSONテキストをセットする
+    out.setContent(JSON.stringify(result));
+  }
 }
