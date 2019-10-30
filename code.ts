70d0402b8587bf05407fd664179ea9cdbafb6116
kooooohe <1211lta+github@gmail.com>
2019-10-30 16:07:50 +0900

    fixed codes

--- code.ts
+++ code.ts
@@ -8,13 +8,12 @@ function doPost(e) {
     "preferred_visit_date",
     "remarks"
   ];
-  let addData: any[][] = [];
+  let addData: any[] = [];
   for (let key of keys) {
-    addData[0].push(e.parameters[key]);
+    addData.push(e.parameters[key]);
   }
   const sheet = SpreadsheetApp.getActive().getSheetByName("testGas");
-  const targetRow = sheet.getLastRow() + 1;
 
   // シートへの書き込み、getRange(開始行、開始列、行数、列数)
-  sheet.getRange(targetRow, 1, 1, addData[0].length).setValues(addData);
+  sheet.appendRow(addData);
 }
